50c8c13be9752c4fbcaf9c3945eab3564d3cc0c8
Rayan Sekkat <rayansekkat@macbook-pro-de-rayan.home>
2021-02-08 10:29:06 +0100

    add frontend

---
+++ frontend/mysepace/src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <img alt="vue logo" src="../assets/logo.png" />
+    <h1>This is the home page</h1>
+    <div>
+      <small>Not logged in?</small>
+      <button type="button" @click="loginClick()">Login</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { login } from "../utils/auth";
+export default {
+  name: "Home",
+  methods: {
+    loginClick() {
+      login();
+    },
+  },
+};
+</script>
