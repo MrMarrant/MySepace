69f3d99e05ff3c6fe83804b1ff15b37dae3e2fd8
Zaelyndra <francoispierrerousseau.44@gmail.com>
2021-02-13 18:48:30 +0100

    Page article plus commentaires

---
+++ frontend/mysepace/src/components/Comment.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="shadow-lg pt-4 ml-2 mr-2 rounded-lg">
+    <a href="#" class="block bg-white py-3 border-t pb-4">
+      <div class="px-4 py-2 flex  justify-between">
+        <span class="text-sm font-semibold text-gray-900">Gloria Roberston</span>
+        <div class="flex">
+          <span class="px-4 text-sm font-semibold text-gray-600"> yesterday</span>
+          <img class="h-6 w-6 rounded-full object-cover"
+               src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=4&amp;w=144&amp;q=80"
+               alt="">
+        </div>
+      </div>
+      <p class="px-4 py-2 text-sm font-semibold text-gray-700">Lorem mmmmm ipsum dolor sit amet consectetur
+        adipisicing elit. Iusto adipisci laudantium <br> iste delectus explicabo id molestiae cupiditate corrupti
+        distinctio alias. <br> Temporibus quae tenetur quod cupiditate, nostrum tempore inventore maxime ut! </p>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Comment"
+};
+</script>
+
+<style scoped>
+
+</style>
--- frontend/mysepace/src/router/index.js
+++ frontend/mysepace/src/router/index.js
@@ -2,6 +2,7 @@ import Profile from "../views/Profile.vue";
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Article from  '../views/Article.vue'
 
 Vue.use(VueRouter)
 
@@ -25,6 +26,11 @@ const router = new VueRouter({
       path: "/profile",
       name: "profile",
       component: Profile
+    },
+    {
+      path: "/article",
+      name: "article",
+      component: Article
     }
   ]
 });
