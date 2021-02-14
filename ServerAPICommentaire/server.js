7ee5a873229e4b2af4b33d4b6660d5aa0189a419
MrMarrant <georgesandalous@gmail.com>
2021-02-14 10:38:18 +0100

    Update des access-controll sur les serveurs api : allow all

--- ServerAPICommentaire/server.js
+++ ServerAPICommentaire/server.js
@@ -13,6 +13,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const db = require("./models");
 db.sequelize.sync();
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    next();
+});
+
 require("./routes/commentaire.routes.js")(app);
 app.listen(PORT, () => {
     console.log("Serveur Commentaire à l'écoute")
